14697c4166e7dcecdb0f837703cc6500ab1181f2
Praneetha2017 <2400032017@kluniversity.in>
2026-01-23 07:54:10 +0530

    23-jan

---
+++ 2400032017-4exp/src/main/java/com/example/demo/MainAppXML.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class MainAppXML {
+	
+
+
+	    public static void main(String[] args) {
+
+	        ApplicationContext context =
+	                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+	        Student student = (Student) context.getBean("student");
+	        student.display();
+	    }
+	}
+
